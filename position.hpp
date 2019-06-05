b107914007253dc38b3d4b75db96d0b0abe71fcf
taotao54321 <taotao54321@gmail.com>
2019-06-05 22:46:06 +0900

    foo

--- position.hpp
+++ position.hpp
@@ -7,19 +7,36 @@
 
 using namespace std;
 
+inline u64 pos_area(u64 point, u64 w, u64 u, u64 d, u64 l, u64 r) {
+    u64 boundary = w | u | d | l | r;
+    u64 seed = board_neighbor(point);
+    return board_floodfill(boundary, seed);
+}
+
 struct Position {
-    u64 player;
+    u64 area;
     u64 wall;
     u64 up;
     u64 down;
     u64 left;
     u64 right;
     u64 piece[9];
-    Position() : player(), wall(), up(), down(), left(), right(), piece() {}
-    Position(u64 p, u64 w, u64 u, u64 d, u64 l, u64 r,
+    Position() : area(), wall(), up(), down(), left(), right(), piece() {}
+    Position(u64 a, u64 w, u64 u, u64 d, u64 l, u64 r,
              u64 c0, u64 c1, u64 c2, u64 c3, u64 c4, u64 c5, u64 c6, u64 c7, u64 c8)
-        : player(p), wall(w), up(u), down(d), left(l), right(r),
+        : area(a), wall(w), up(u), down(d), left(l), right(r),
           piece{c0,c1,c2,c3,c4,c5,c6,c7,c8} {}
+
+    static Position from_point(
+        u64 point, u64 w, u64 u, u64 d, u64 l, u64 r,
+        u64 c0, u64 c1, u64 c2, u64 c3, u64 c4, u64 c5, u64 c6, u64 c7, u64 c8)
+    {
+        return Position {
+            pos_area(point,w,u,d,l,r),
+            w, u, d, l, r,
+            c0, c1, c2, c3, c4, c5, c6, c7, c8
+        };
+    }
 };
 
 enum PositionResult {
@@ -30,46 +47,48 @@ enum PositionResult {
 
 inline Position pos_read(const string& s) {
     istringstream in(s);
-    Position pos;
 
     int player_x,player_y; in >> player_x >> player_y;
-    pos.player = 1ULL << yx2idx(player_y,player_x);
+    u64 player_point = 1ULL << yx2idx(player_y,player_x);
 
     vector<string> grid(8);
     REP(y, 8) {
         in >> grid[y];
     }
 
+    u64 w=0, u=0, d=0, l=0, r=0;
+    u64 piece[9] {};
     REP(y, 8) REP(x, 8) {
         char c = grid[y][x];
         int  i = yx2idx(y,x);
         if(c == '#') {
-            pos.wall |= 1ULL << i;
+            w |= 1ULL << i;
         }
         else if(c == '^') {
-            pos.up |= 1ULL << i;
+            u |= 1ULL << i;
         }
         else if(c == 'v') {
-            pos.down |= 1ULL << i;
+            d |= 1ULL << i;
         }
         else if(c == '<') {
-            pos.left |= 1ULL << i;
+            l |= 1ULL << i;
         }
         else if(c == '>') {
-            pos.right |= 1ULL << i;
+            r |= 1ULL << i;
         }
         else if('0' <= c && c <= '8') {
-            pos.piece[c-'0'] |= 1ULL << i;
+            piece[c-'0'] |= 1ULL << i;
         }
     }
 
-    return pos;
+    return Position::from_point(
+        player_point,
+        w, u, d, l, r,
+        piece[0], piece[1], piece[2], piece[3], piece[4], piece[5], piece[6], piece[7], piece[8]
+    );
 }
 
 inline string pos_str(const Position& pos) {
-    int player_i = board_bsf(pos.player);
-    int player_y,player_x; tie(player_y,player_x) = idx2yx(player_i);
-
     vector<string> grid(8, string(8,'.'));
 
     REP(y, 8) REP(x, 8) {
@@ -100,26 +119,19 @@ inline string pos_str(const Position& pos) {
     }
 
     ostringstream out;
-    out << player_x << ' ' << player_y << '\n';
     for(const auto& s : grid)
         out << s << '\n';
     return out.str();
 }
 
-inline u64 pos_area(const Position& pos) {
-    u64 boundary = pos.wall | pos.up | pos.down | pos.left | pos.right;
-    u64 seed = board_neighbor(pos.player);
-    return board_floodfill(boundary, seed);
-}
-
 // UDLR
 inline tuple<u64,u64,u64,u64> pos_moves(const Position& pos) {
-    u64 area = pos_area(pos);
-    u64 up    = pos.up & area;
-    u64 down  = pos.down & area;
-    u64 left  = pos.left & area;
-    u64 right = pos.right & area;
-    return make_tuple(up, down, left, right);
+    return make_tuple(
+        pos.area & pos.up,
+        pos.area & pos.down,
+        pos.area & pos.left,
+        pos.area & pos.right
+    );
 }
 
 inline PositionResult pos_result(const Position& pos) {
@@ -131,15 +143,35 @@ inline PositionResult pos_result(const Position& pos) {
     return SOLVED;
 }
 
-inline void pos_erase(Position& pos) {
-    for(u64& e : pos.piece) {
-        e &= ~board_neighbor(e);
-    }
+inline Position pos_move_make(
+    u64 point,
+    u64 w, u64 u, u64 d, u64 l, u64 r,
+    u64 c0, u64 c1, u64 c2, u64 c3, u64 c4, u64 c5, u64 c6, u64 c7, u64 c8)
+{
+    u64 boundary = w | u | d | l | r;
+    u64 seed = board_neighbor(point);
+    u64 area = board_floodfill(boundary, seed);
+
+    // erase
+    c0 &= ~board_neighbor(c0);
+    c1 &= ~board_neighbor(c1);
+    c2 &= ~board_neighbor(c2);
+    c3 &= ~board_neighbor(c3);
+    c4 &= ~board_neighbor(c4);
+    c5 &= ~board_neighbor(c5);
+    c6 &= ~board_neighbor(c6);
+    c7 &= ~board_neighbor(c7);
+    c8 &= ~board_neighbor(c8);
+
+    return Position {
+        area, w, u, d, l, r,
+        c0, c1, c2, c3, c4, c5, c6, c7, c8
+    };
 }
 
 inline Position pos_move_up(const Position& pos, u64 point) {
     int x = board_bsf(point) % 8;
-    Position res {
+    return pos_move_make(
         point,
         board_rotate_up(pos.wall, x),
         board_rotate_up(pos.up, x),
@@ -154,15 +186,13 @@ inline Position pos_move_up(const Position& pos, u64 point) {
         board_rotate_up(pos.piece[5], x),
         board_rotate_up(pos.piece[6], x),
         board_rotate_up(pos.piece[7], x),
-        board_rotate_up(pos.piece[8], x),
-    };
-    pos_erase(res);
-    return res;
+        board_rotate_up(pos.piece[8], x)
+    );
 }
 
 inline Position pos_move_down(const Position& pos, u64 point) {
     int x = board_bsf(point) % 8;
-    Position res {
+    return pos_move_make(
         point,
         board_rotate_down(pos.wall, x),
         board_rotate_down(pos.up, x),
@@ -177,15 +207,13 @@ inline Position pos_move_down(const Position& pos, u64 point) {
         board_rotate_down(pos.piece[5], x),
         board_rotate_down(pos.piece[6], x),
         board_rotate_down(pos.piece[7], x),
-        board_rotate_down(pos.piece[8], x),
-    };
-    pos_erase(res);
-    return res;
+        board_rotate_down(pos.piece[8], x)
+    );
 }
 
 inline Position pos_move_left(const Position& pos, u64 point) {
     int y = board_bsf(point) / 8;
-    Position res {
+    return pos_move_make(
         point,
         board_rotate_left(pos.wall, y),
         board_rotate_left(pos.up, y),
@@ -200,15 +228,13 @@ inline Position pos_move_left(const Position& pos, u64 point) {
         board_rotate_left(pos.piece[5], y),
         board_rotate_left(pos.piece[6], y),
         board_rotate_left(pos.piece[7], y),
-        board_rotate_left(pos.piece[8], y),
-    };
-    pos_erase(res);
-    return res;
+        board_rotate_left(pos.piece[8], y)
+    );
 }
 
 inline Position pos_move_right(const Position& pos, u64 point) {
     int y = board_bsf(point) / 8;
-    Position res {
+    return pos_move_make(
         point,
         board_rotate_right(pos.wall, y),
         board_rotate_right(pos.up, y),
@@ -223,9 +249,7 @@ inline Position pos_move_right(const Position& pos, u64 point) {
         board_rotate_right(pos.piece[5], y),
         board_rotate_right(pos.piece[6], y),
         board_rotate_right(pos.piece[7], y),
-        board_rotate_right(pos.piece[8], y),
-    };
-    pos_erase(res);
-    return res;
+        board_rotate_right(pos.piece[8], y)
+    );
 }
 
